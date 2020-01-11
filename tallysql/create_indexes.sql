eb679788fd8c0cb476e6c4547c67c9770b1cdd80
Nov05 <arwen.liuwenjing@gmail.com>
2020-01-10 19:59:13 -0600

    .

--- tallysql/create_indexes.sql
+++ tallysql/create_indexes.sql
@@ -1,11 +1,9 @@
-CREATE INDEX idx_review ON tallyds.review (business_id, datetime DESC);
+-- CREATE INDEX idx_review ON tallyds.review (business_id, datetime DESC);
 
--- select * from tallyds.review 
--- where business_id = 'Iq7NqQD-sESu3vr9iEGuTA'
--- and datetime <= '2018-11-30'
--- and datetime >= '2017-11-30';
+select * from tallyds.review 
+where business_id = 'Iq7NqQD-sESu3vr9iEGuTA'
+and datetime <= '2018-11-30'
+and datetime >= '2017-11-30';
 
--- select * from tallyds.review 
--- where business_id = 'aunt-jakes-new-york'
--- and datetime <= '2018-11-30'
--- and datetime >= '2017-11-30';
+select * from tallyds.business
+where alias = 'aunt-jakes-new-york';
